b7b380e90906b1803054253ad1403dad4aa05828
poc7667 <poc7667@gmail.com>
2014-11-13 23:34:17 +0800

    Update weather parse

--- lib/tasks/nooa_weather_parser.py
+++ lib/tasks/nooa_weather_parser.py
@@ -29,8 +29,16 @@ import json
 from awesome_print import ap
 from ish_parser import ish_parser
 from ish_report import ish_report, ish_reportException
+import glob
 # Load custom modules
 # -----------------------------------------------------------------
+def get_source_files():
+  source_files = []
+  for file in glob.glob('./*'):
+    if '.json' not in file:
+        source_files.append(file)
+  return source_files
+
 
 def setup_logging(logging_file_path="log/stdout.log"):
     log_dir = os.path.dirname(os.path.realpath(logging_file_path))
@@ -47,39 +55,46 @@ def get_precipitation(report):
     else:
         return ["null","null"]
 
+def convert_file_to_json(sample_file):
+    export_weather_list = []
+    setup_logging()
+    with open(sample_file) as fp:
+        content = fp.read()
+    wp = ish_parser()
+    wp.loads(content)
+    try:
+        reports = wp.get_reports()
+    except BaseException:
+        pass
+    for (i, report) in enumerate(reports):
+        precip_hour, precip_depth = get_precipitation(report)
+        current_weather = {
+            "report_type": str(report.report_type),
+            "latitude": str(report.latitude),
+            "longitude": str(report.longitude),
+            "datetime": str(report.datetime),
+            "air_temperature": str(report.air_temperature),
+            "wban": str(report.wban),
+            "weather_station": str(report.weather_station),
+            "sea_level_pressure": str(report.sea_level_pressure),
+            "dew_point_temperature": str(report.dew_point_temperature),
+            "elevation": str(report.elevation),
+            "visibility_distance": str(report.visibility_distance),
+            "precipitation_hour": str(precip_hour),
+            "precipitation_depth_in_mm": str(precip_depth)
+        }
+        pdb.set_trace()
+        export_weather_list.append(current_weather)
+    with open(sample_file+'.json', 'w') as f:
+        json.dump(export_weather_list, f)
 def main():
     try:
-        SAMPLE_FILE = '999999-94996-2014.min'
-        export_weather_list = []
-        setup_logging()
-        with open(SAMPLE_FILE) as fp:
-            content = fp.read()
-        wp = ish_parser()
-        wp.loads(content)
-        try:
-            reports = wp.get_reports()
-        except BaseException  as e:
-            pass
-        for (i, report) in enumerate(reports):
-            precip_hour, precip_depth = get_precipitation(report)
-            current_weather = {
-                "report_type": str(report.report_type),
-                "latitude": str(report.latitude),
-                "longitude": str(report.longitude),
-                "datetime": str(report.datetime),
-                "air_temperature": str(report.air_temperature),
-                "wban": str(report.wban),
-                "weather_station": str(report.weather_station),
-                "sea_level_pressure": str(report.sea_level_pressure),
-                "dew_point_temperature": str(report.dew_point_temperature),
-                "elevation": str(report.elevation),
-                "visibility_distance": str(report.visibility_distance),
-                "precipitation_hour": str(precip_hour),
-                "precipitation_depth_in_mm": str(precip_depth)
-            }
-            export_weather_list.append(current_weather)
-        with open('sample_weather.json', 'w') as f:
-            json.dump(export_weather_list, f)
+        target_yearly_folders = ["./2014"]
+        for folder in target_yearly_folders:
+            os.chdir(folder)
+            for source_file in  get_source_files():
+                convert_file_to_json(source_file)
+            os.chdir("../")
 
     except BaseException as e:
         traceback.print_exc(file=sys.stdout)
